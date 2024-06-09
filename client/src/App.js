3636138c08dd79724773f1b7576e17172b68b346
trungnguyenthien <ngthientrung@gmail.com>
2024-06-09 18:59:19 +0700

    .

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,12 @@ import Help from './pages/Help';
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/help">Help</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/help" element={<Help />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </div>
     </Router>
   );
 }
